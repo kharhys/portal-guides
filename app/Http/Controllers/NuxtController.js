794413218b7642272c8a9a599b533cc83f89d089
kelvin kharhys <nrhyska@gmail.com>
2017-05-26 09:46:17 +0300

    fix nuxt initialize in controller

--- app/Http/Controllers/NuxtController.js
+++ app/Http/Controllers/NuxtController.js
@@ -4,15 +4,40 @@ const Env = use('Env')
 const Config = use('Config')
 const Nuxt = require('nuxt')
 
+const alwaysDone = require('always-done')
+
 class NuxtController {
 
   constructor () {
-    let config = Config.get('nuxt')
-    config.dev = Env.get('NODE_ENV') === 'development'
-    this.nuxt = new Nuxt(config)
-    if (Env.get('NODE_ENV') === 'development') {
-      this.nuxt.build()
-    }
+    // let config = Config.get('nuxt')
+    // config.dev = Env.get('NODE_ENV') === 'development'
+    // this.nuxt = new Nuxt(config)
+    // if (Env.get('NODE_ENV') === 'development') {
+    //   this.nuxt.build()
+    // }
+    let ctx = this;
+    alwaysDone(async function () {
+      // let config = Config.get('nuxt')
+      // config.dev = Env.get('NODE_ENV') === 'development'
+      // this.nuxt = new Nuxt(config)
+      // if (Env.get('NODE_ENV') === 'development') {
+      //   this.nuxt.build()
+      // }
+
+      let config = Config.get('nuxt')
+      config.dev = Env.get('NODE_ENV') === 'development'
+      console.log('config', config)
+
+      let nuxt = await new Nuxt(config)
+      ctx.nuxt = nuxt
+
+      if (config.dev ) { await nuxt.build()  }      
+      return nuxt
+
+    }, function done (e, res) {
+      console.log('res', res) 
+    })
+
   }
 
   * render (request, response) {
