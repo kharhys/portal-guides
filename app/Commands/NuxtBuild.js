51d003e2734e7be9b87872b4677bf3f9f86d47ce
kelvin kharhys <nrhyska@gmail.com>
2017-05-26 09:29:31 +0300

    control flow using always-done

--- app/Commands/NuxtBuild.js
+++ app/Commands/NuxtBuild.js
@@ -4,6 +4,8 @@ const Command = use('Command')
 const Config = use('Config')
 const Nuxt = require('nuxt')
 
+const alwaysDone = require('always-done')
+
 class NuxtBuild extends Command {
 
   /**
@@ -40,12 +42,23 @@ class NuxtBuild extends Command {
     // console.log('this.nuxt ==>', this.nuxt)
     // this.info('Building nuxt.js application...')
     // yield this.nuxt.build()
-    (new Nuxt(config)).then(nuxt => {
-      this.nuxt = nuxt
-      console.log('this.nuxt ==>', this.nuxt)
-      this.info('Building nuxt.js application...')
-      this.nuxt.build()
+
+    // (new Nuxt(config)).then(nuxt => {
+    //   this.nuxt = nuxt
+    //   console.log('this.nuxt ==>', this.nuxt)
+    //   this.info('Building nuxt.js application...')
+    //   this.nuxt.build()
+    // })
+
+    alwaysDone(async function () {
+      console.log('config', config)
+      let nuxt = await new Nuxt(config)
+      await nuxt.build()
+      return nuxt
+    }, function done (e, res) {
+      console.log('res', res) // => 'foobar' 
     })
+
   }
 
 }
